d804b15359426e23216b2ef64ef2b97bf7f3c7b1
Shahab6699 <168788928+Shahab6699@users.noreply.github.com>
2024-08-22 04:52:37 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'f0879971-05d9-448d-aae6-786394c768c1';
+let userID = '50d4e733-178e-4dcf-b087-45918c10b9b6';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
